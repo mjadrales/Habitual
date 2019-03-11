c233c2df176ba522853ae990d351e3689aa8c85e
Morgan Adrales <mjadrales12@gmail.com>
2019-03-10 21:49:42 -0500

    Iteration #2 Commit
    -Fleshed out UI for each page

---
+++ app/src/main/java/com/example/habitual/FragmentGoals.java
@@ -0,0 +1,22 @@
+package com.example.habitual;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentGoals extends Fragment {
+    View view;
+    public FragmentGoals() { }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.goals_fragment, container, false);
+        return view;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/habitual/FragmentLogging.java
@@ -0,0 +1,20 @@
+package com.example.habitual;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentLogging extends Fragment {
+    View view;
+    public FragmentLogging() { }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.logging_fragment, container, false);
+        return view;
+    }
+}
